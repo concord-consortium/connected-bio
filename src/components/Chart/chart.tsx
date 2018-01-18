500423cc2fef6ef84cba3ad248fae94a404bc9d0
Eli Kosminsky <ekosminsky@concord.org>
2018-01-17 19:25:41 -0800

    Fixing compiler errors

--- src/components/Chart/chart.tsx
+++ src/components/Chart/chart.tsx
@@ -17,32 +17,33 @@ class Chart extends React.Component<any, any> {
     let values = Object.keys(activeSubstances).map(function(e) {
       return activeSubstances[e]
     })
-    let data = Object.assign({}, this.baseData);
+    let data:any = Object.assign({}, this.baseData);
     data.labels = Object.keys(activeSubstances);
     data.datasets[0].data = values;
     data.datasets[0].label = activeAssay;
+    let options:any = {
+      title:{
+        display:true,
+        text:'Substance Breakdown',
+        fontSize:25
+      },
+      legend:{
+        display:true,
+        position:"bottom"
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    };
     return (
       <div className="chart">
         <HorizontalBar
           data={data}
-          options={{
-            title:{
-              display:true,
-              text:'Substance Breakdown',
-              fontSize:25
-            },
-            legend:{
-              display:true,
-              position:"bottom"
-            },
-            scales: {
-              xAxes: [{
-                ticks: {
-                  beginAtZero:true
-                }
-              }]
-            }
-          }}
+          options={options}
         />
       </div>
     )
