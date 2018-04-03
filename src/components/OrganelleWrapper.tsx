35c900afcf9647f1b509db79423cccd6ac3aafc9
Eli Kosminsky <ekosminsky@concord.org>
2018-04-03 15:05:32 -0700

    Bumping g-proteins from 5 back down to 3

--- src/components/OrganelleWrapper.tsx
+++ src/components/OrganelleWrapper.tsx
@@ -121,7 +121,7 @@ class OrganelleWrapper extends React.Component<OrganelleWrapperProps, OrganelleW
 
     this.model.setTimeout(
       () => {
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < 3; i++) {
           this.model.world.createAgent(this.model.world.species.gProtein);
         }
       },
